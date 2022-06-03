b847e58366e9aea7bb7293f339c1980b8a615898
James <JamesPGrant1999@gmail.com>
2022-06-03 01:53:19 -0400

    almost finished

--- test2.js
+++ test2.js
@@ -44,7 +44,7 @@ Book.prototype.notRead = function(){
 }
 
 function addANewBook(){
-    const book = new Book(TITLE.value, AUTHOR.value, PAGES.value);
+    const book = new Book(TITLE.value, AUTHOR.value, PAGES.value, this.notRead);
     myLibrary.push(book);
     library2.push(book)
     console.log(library2)
@@ -55,13 +55,14 @@ function addANewBook(){
 function displayBook(){
     for(const book of myLibrary){
         const CARDCLONE = CARD.cloneNode(true);
-        CARDCLONE.setAttribute('data-attribute', `${library2.indexOf(book)}`)
+        CARDCLONE.setAttribute('data-book', `${library2.indexOf(book)}`)
         const TITLECLONE = newTitle.cloneNode(true);
         const PAGESCLONE = newPages.cloneNode(true);
         const  AUTHORCLONE = newAuthor.cloneNode(true);
         const REMOVEBUTTON =document.createElement('button');
         const READBUTTON = document.createElement('button');
         READBUTTON.classList.add('btn', 'btn-danger', 'read')
+        READBUTTON.setAttribute('data-book', `${library2.indexOf(book)}`)
         READBUTTON.setAttribute('id', 'read')
         CONTAINED.appendChild(CARDCLONE)
         CARDCLONE.appendChild(TITLECLONE)
@@ -77,24 +78,52 @@ function displayBook(){
         REMOVEBUTTON.textContent= `Remove`
         READBUTTON.textContent = `Not Read`
         REMOVEBUTTON.addEventListener('click', removeBook)
+        READBUTTON.addEventListener('click', readToggle)
         myLibrary.pop(book)
     }
 }
 
-function removeBook(){
-    addANewBook()
-    const CARDCLONE =document.querySelector('[data-attribute]')
-    CONTAINED.removeChild(CARDCLONE)
-    console.log(myLibrary)
+function removeBook(e){
+    let book = e.currentTarget.parentNode
+    console.log(book)
+    book.remove()
     for(const book in myLibrary){
         myLibrary.splice(book)
     }
+    console.log(myLibrary)
 }
+
 function readToggle(){
     const READBUTTON = document.querySelector('#read')
-    READBUTTON.addEventListener('click', ()=>{
-        
-    })
+    for(let i = 0; i < library2.length; i++){
+        if ( this.read === this.notRead){
+            READBUTTON.textContent = `Read`
+            READBUTTON.classList.remove('btn', 'btn-danger', 'read')
+            READBUTTON.classList.add('btn','btn-success', 'read')
+            READBUTTON.addEventListener('click', notReadToggle)
+           return this.read === this.didRead
+        }
+            
+        }
+        console.log(this.read)
+    }
+function notReadToggle(){
+    for(let i = 0; i < library2.length; i++){
+    const READBUTTON = document.querySelector('#read')
+        if(this.read === this.didRead){
+            READBUTTON.textContent = `Not Read`;
+            READBUTTON.classList.remove('btn','btn-success', 'read')
+            READBUTTON.classList.add('btn', 'btn-danger', 'read')
+            READBUTTON.removeEventListener('click', notReadToggle)
+            return this.read === this.notRead
+            console.log(this.read)
+        }
+        }
+    }
+
+function populateStorage(){
+    localStorage.setItem('library2')
 }
 BUTTON.addEventListener('click', addANewBook);
 BUTTON.addEventListener('click',displayBook);
+BUTTON.addEventListener('click',populateStorage);
